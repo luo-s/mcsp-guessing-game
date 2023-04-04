f42655d4e575106fcea1196b8244884b847cad90
Shu <luo.s@outlook.com>
2023-04-04 09:21:22 -0800

    final commit

--- app.js
+++ app.js
@@ -3,77 +3,106 @@ input: string (prompt)
 output: string (alert)
 */
 
-//Set the answer
-let answer = 5 //Math.floor(Math.random() * 10 + 1);
-
-//input
-var name = prompt("What is your name?");
-var input = prompt("Please enter an  integer from 1 to 10");
-
-// edge cases: Number(true) === 1; Number(false) === Number(' ') === Number(null) === 0;
-
+//Define Variables First
+var names;
+var answer;
+var input;
 var count = 0;
 var guesses = {};
 var regame;
-function play() {
-    while (true) {
-        if (input === null) {
-            alert('See you next time!');
-            break;
-        }
-        var num = Number(input); //convert string to number
-        var isSV = (input.trim() === ''); //is it a special value?
-        var isI = Number.isInteger(num) === true; //is it an integer?
-        if (isSV || (!isI)) {
-            input = prompt('Please enter a VALID integer between 1 and 10');
-        } else {
-            count += 1;
-            if (num > answer) {
-                input = prompt(`Sorry ${name}, Guess Lower`);
-            } else if (num < answer) {
-                input = prompt(`Sorry ${name}, Guess Higher`);
-            } else {
-                if (guesses.name === undefined) {
-                    alert( `That\'s Correct ${name}! It only took you ${count} guesses!`);
-                    guesses.name = count;
-                } else {
-                    if (count < guesses.name) {
-                        var dif = guesses.name - count;
-                        guesses.name = count;
-                        alert(`That's Correct ${name}! And you beat your personal best by ${dif} fewer guess!`);
-                    } else if (count > guesses.name) {
-                        dif = count - guesses.name;
-                        alert(`That's Correct ${name}! And you miss your personal best by ${dif} fewer guess!`);
-                    }
-                }
-                
-                while (true) {
-                    regame = prompt("Would you like to play again? Yes/No?");
-                    if (regame === null || regame === 'No') {
-                        console.log(regame);
-                        alert('See you next time!!');
-                        return;
-                    } else if (regame !== 'Yes') {
-                        console.log(regame);
-                        regame = prompt("Please enter only 'Yes' or 'No'.");
-                        if (regame === 'No') {
-                            alert('See you next time!!!');
-                            return;
-                        } else if (regame === 'Yes') {
-                            input = prompt('Please enter an Integer between 1 and 10.');
-                            count = 0;
-                            break;
-                        }
-                    } else if (regame === 'Yes') {
-                        input = prompt('Please enter an Integer between 1 and 10.');
-                        count = 0;
-                        break;
-                    }
-                }
-            }           
-        }
+var dif;
+//edge-cases: Number(true) === 1; Number(false) === Number(' ') === Number(null) === 0;
+//validate user input (str) -> return (num)
+function inputValid(str) {
+  while (true) {
+    if (str === null) {
+      alert("See you next time!");
+      return false;
     }
+    var num = Number(str); //convert string to number
+    var isSV = str.trim() === ""; //is it a special value?
+    var isI = Number.isInteger(num) === true; //is it an integer?
+    if (isSV || !isI) {
+      str = prompt("Please enter a VALID integer between 1 and 10");
+    } else {
+      count++;
+      return num;
+    }
+  }
 }
+//check the answer: if not correct -> return input; if correct -> return replay option.
+function check(secretN, n) {
+  if (n > secretN) {
+    return (input = prompt(`Sorry ${names}, Guess Lower`));
+  } else if (n < secretN) {
+    return (input = prompt(`Sorry ${names}, Guess Higher`));
+  } else if (n === secretN) {
+    if (guesses[`${names}`] === undefined) {
+      alert(`That\'s Correct ${names}! It only took you ${count} guesses!`);
+      guesses[`${names}`] = count;
+    } else {
+      if (count < guesses[`${names}`]) {
+        dif = guesses[`${names}`] - count;
+        guesses[`${names}`] = count;
+        alert(
+          `That's Correct ${names}! And you beat your personal best by ${dif} fewer guess!`
+        );
+      } else if (count > guesses[`${names}`]) {
+        dif = count - guesses[`${names}`];
+        alert(
+          `That's Correct ${names}! And you miss your personal best by ${dif} more guess!`
+        );
+      } else {
+        alert(
+          `That's Correct ${names}! And you did the same as your personal best!`
+        );
+      }
+    }
+    console.log(guesses);
+    return true;
+  }
+}
+//validate regame input (str) -> boolean: true (replay) false (quit);
+function regameValid(str) {
+  while (true) {
+    if (str === null || str === "N" || str === "n") {
+      alert("See you next time!!");
+      return false;
+    } else if (str !== "Y" && str !== "y") {
+      str = prompt("Please enter only 'Y' or 'N'.");
+    } else {
+      count = 0;
+      return true;
+    }
+  }
+}
+//replay
+function replay() {
+  while (true) {
+    regame = prompt("Would you like to play one more time? Y/N?");
+    if (regameValid(regame) === true) {
+      play();
+    } else {
+      return;
+    }
+  }
+}
+//play
+function play() {
+  answer = Math.floor(Math.random() * 10 + 1);
+  names = prompt("Welcome! What is your name?");
+  input = prompt("Please enter an integer from 1 to 10");
+  while (true) {
+    var number = inputValid(input);
+    if (number === false) {
+      break;
+    }
+    if (check(answer, number) === true) {
+      break;
+    }
+  }
+  return;
+}
+//execution
 play();
-
-
+replay();
